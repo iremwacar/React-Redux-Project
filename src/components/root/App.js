eaa4b3b0faf5ba1a0c39e5db75c4d0503605e10f
irem acar <iremmacar35@gmail.com>
2024-06-30 20:24:58 +0300

    basic commit

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {Container} from "reactstrap"
+import Navi from '../navi/Navi'
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div >
-    
-    </div>
+    <Container>
+      <Navi/>
+      <Dashboard/>
+    </Container>
   );
 }
 
